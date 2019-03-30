c4dd7e2d11c01ec996f574d37d584b7fb7944ea6
root <root@ip-172-31-90-170.ec2.internal>
2019-03-30 04:53:09 +0000

    Add2

---
+++ List_loop.py
@@ -0,0 +1,12 @@
+users =[]
+users.append("ahmad")
+users.append("john")
+users.append("linda")
+users.insert(1,"mary")
+users.append("john")
+print (users)
+#Sorting a list
+#users.sort() #This will sort a list
+print (sorted(users))
+print (users)
+sorted_users = sorted(users)
--- list_demo.py
+++ list_demo.py
@@ -1,6 +1,18 @@
 users = ["user1","user2","user3"]
-print(users[0])
-#accessing last elemments of list
-print(users[len(users)-1])
-print(users[-1])
-print(users[-2])
+print (users[0])
+ #Accessing last element in list
+print (users[len(users)-1])
+print (users[-1])
+print (users[-2])
+#add and move elements in the list
+users[1]="Anis"
+print (users)
+#This will append the list with new user
+users.append("Peter")
+print(users)
+
+# insert add an item at specific index
+users.insert(1, "mary")
+users.remove("Peter")
+print(users)
+
