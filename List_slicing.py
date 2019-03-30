e44866842467d515265c5f08c17b96e1e45bb8e1
root <root@ip-172-31-90-170.ec2.internal>
2019-03-30 05:19:03 +0000

    aa

---
+++ List_slicing.py
@@ -0,0 +1,8 @@
+#List slicing
+
+users=["User1","User2","User3","User4"]
+print (users[1::2])
+print (users[1:])  #starting from second last
+print (users[1:-1])
+print (users[:3]) #Print the first third items
+print (users[::-1]) #Print in revers order
